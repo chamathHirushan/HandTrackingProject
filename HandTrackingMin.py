85fc3517f3058a0085e17ded2b3d84d7d7d83ccd
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2024-08-21 12:14:04 +0530

    displaying fps

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -8,6 +8,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()  # only takes RGB color images as inputs
 mpDraw = mp.solutions.drawing_utils
 
+pTime = 0  # previous time
+cTime = 0  # current time
+
 while True:
     success, img = cap.read()  # read a frame
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # get the RGB Image
@@ -19,6 +22,13 @@ while True:
         for handLandMarks in results.multi_hand_landmarks:  # extract the information of each hand
             # draw on the original image
             mpDraw.draw_landmarks(img, handLandMarks, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()  # logic to calculate the fps
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70),
+                cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
 
     cv2.imshow("image", img)  # show the image
     cv2.waitKey(1)  # wait for 1ms
